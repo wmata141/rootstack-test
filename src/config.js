80a10f53165df92b697743c39ff1c905bcdac080
William Mata <wmata0141@gmail.com>
2021-04-27 06:49:30 -0400

    global config to deploy

---
+++ src/config.js
@@ -0,0 +1,5 @@
+require('dotenv').config(); 
+
+export const URL = process.env.REACT_APP_URL
+// export const API_KEY = process.env.REACT_APP_API_KEY
+// export const HASH = process.env.REACT_APP_HASH
